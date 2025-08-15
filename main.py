b830d6ea81eff39672360a155f8b4c07761d9315
MrHelfer <elias.helfer@gmail.com>
2025-08-15 20:30:47 +0200

    add count chars

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+from stats import count_num_words
+from stats import count_chars
+
 def get_book_text(filepath):
     with open(filepath) as f:
         file_contents = f.read()
     return str(file_contents)
 
-def count_num_words(input_text):
-    word_list = input_text.split()
-    num_words = len(word_list)
-    return num_words
-
+    
 def main():
     text = (get_book_text("./books/frankenstein.txt"))
     count = count_num_words(text)
+    frequency = count_chars(text)
     print(f"{count} words found in the document")
+    print(frequency)
 
 main()
--- stats.py
+++ stats.py
@@ -2,3 +2,13 @@ def count_num_words(input_text):
     word_list = input_text.split()
     num_words = len(word_list)
     return num_words
+
+def count_chars(input_text):
+    frequency = {}
+    ready_text = input_text.lower()
+    for char in ready_text:
+        if char in frequency:
+            frequency[char] += 1
+        else:
+            frequency[char] = 1
+    return frequency
